65fde211c907a9dbecbed89aaeed880decdda122
Eduardo Ferro <eferro@alea-soluciones.com>
2013-05-25 09:30:57 +0200

    Test for basic execute

--- boscli/interpreter.py
+++ boscli/interpreter.py
@@ -1,8 +1,16 @@
 from boscli import exceptions
 
 class Interpreter(object):
+	def __init__(self):
+		self._commands = []
+
 	def add_command(self, command):
-		pass
+		self._commands.append(command)
 
 	def eval(self, line_text):
+		for command in self._commands:
+			if command.match(line_text):
+				command.execute()
+				return
+
 		raise exceptions.NotMatchingCommandFound(line_text)
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,17 +1,29 @@
 import unittest
+from doublex import *
 
 import boscli
 from boscli import exceptions
 
 # Initial tests:
 # Eval empty line
-# Eval line not matching any command
 # Eval line matching a command
 
 IRRELEVANT_LINE = 'irrelevant_line'
 
 class InterpreterTest(unittest.TestCase):
-	def test_exception_raised_when_evaluating_a_line_not_matching_any_command(self):
 
+	def test_exception_raised_when_evaluating_a_line_not_matching_any_command(self):
 		interpreter = boscli.Interpreter()
+	
 		self.assertRaises(exceptions.NotMatchingCommandFound, interpreter.eval, IRRELEVANT_LINE)
+
+	def test_execute_the_command_when_match_found(self):
+		interpreter = boscli.Interpreter()
+
+		with Spy() as command:
+			command.match(IRRELEVANT_LINE).returns(True)
+		interpreter.add_command(command)
+			
+		interpreter.eval(IRRELEVANT_LINE)
+		
+		assert_that(command.execute, called())
