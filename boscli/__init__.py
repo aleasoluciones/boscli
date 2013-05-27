dc2a3106f1e79c9e3e557eb33110e8940d7f9414
eferro <eduardo.ferro.aldama@gmail.com>
2013-05-28 01:51:43 +0200

    utf8 codec for the sources

--- boscli/__init__.py
+++ boscli/__init__.py
@@ -1,2 +1,4 @@
+# -*- coding: utf-8 -*-
+
 from boscli.interpreter import Interpreter
 from boscli.command import Command
--- boscli/interpreter.py
+++ boscli/interpreter.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from boscli import exceptions
 
 class Interpreter(object):
