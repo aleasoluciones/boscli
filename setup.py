3fe797c23a14187a02d282513a941abd609eb5cd
Eduardo Ferro <eferro@alea-soluciones.com>
2013-05-29 17:26:05 +0200

    Setup inicial

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='boscli',
+      version='0.0.1',
+      author='Eduardo Ferro Aldama',
+      description ='',
+      platforms = 'Linux',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+)
