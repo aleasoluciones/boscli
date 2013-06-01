ff0316c041c443443ccc5641fe9ccde91efe6b07
eferro <eduardo.ferro.aldama@gmail.com>
2013-06-01 17:19:10 +0200

    Using shlex for lexical scanner

--- boscli/parser.py
+++ boscli/parser.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import six
-import token, tokenize
+import shlex
 
 
 class Parser(object):
 
 	def parse(self, input_line):
-		tokens = []
-		rawstr = six.StringIO(input_line)
-		for i, item in enumerate(tokenize.generate_tokens(rawstr.readline)):
-			_, token_text, (_, _), (_, _), _ = item
-			if token_text:
-				if token_text.startswith("'") or token_text.startswith('"'):
-					token_text = token_text[1:-1]
-				tokens.append(token_text)
-		return tokens
+		return shlex.split(input_line)
+	
