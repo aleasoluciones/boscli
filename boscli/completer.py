fa1ec0404b61011647347a6f833e4036a346aebb
eferro <eduardo.ferro.aldama@gmail.com>
2013-06-08 09:34:30 +0200

    Refactor

---
+++ boscli/completer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Completer(object):
+	def __init__(self, interpreter, parser):
+		self.interpreter = interpreter
+		self.parser = parser
+	
+	def complete(self, line_to_complete):
+		completions = set()
+		tokens = self.parser.parse(line_to_complete)
+		previous_tokens = tokens[:-1]
+		for command in self.interpreter.active_commands():
+			if command.partial_match(previous_tokens):
+				completions.update(command.complete(tokens))
+		return completions
