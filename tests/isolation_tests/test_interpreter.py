dceec9a00e24db20235d714b15edea8174ab8d02
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2013-09-19 00:10:01 +0200

    Migration test from isolation unit tests to unit tests

--- tests/isolation_tests/test_interpreter.py
+++ tests/isolation_tests/test_interpreter.py
@@ -26,26 +26,6 @@ class InterpreterTest(unittest.TestCase):
         self.parser = Spy(parser.Parser)
         self.interpreter = boscli.Interpreter(self.parser)
 
-    def test_exception_raised_when_evaluating_a_line_not_matching_any_command(self):
-        self.assertRaises(
-            exceptions.NotMatchingCommandFoundError, self.interpreter.eval, IRRELEVANT_LINE)
-
-    def test_empty_line_evaluation_returns_none(self):
-        assert_that(self.interpreter.eval(''), is_(None))
-
-    def test_exception_raised_when_more_than_one_command_match(self):
-        with Spy(boscli.Command) as command1:
-            command1.match(ANY_ARG).returns(True)
-            command1.matching_parameters(ANY_ARG).returns([])
-        with Spy(boscli.Command) as command2:
-            command2.match(ANY_ARG).returns(True)
-            command2.matching_parameters(ANY_ARG).returns([])
-
-        self.interpreter.add_command(command1)
-        self.interpreter.add_command(command2)
-
-        self.assertRaises(
-            exceptions.AmbiguousCommandError, self.interpreter.eval, IRRELEVANT_KEY)
 
     def test_execute_the_command_when_match_found(self):
         with self.parser:
---
+++ tests/unit_tests/test_autocomplete.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from hamcrest import *
+from doublex import *
+
+import boscli
+from boscli import exceptions, basic_types
+from boscli import parser as parser_module
+from boscli import interpreter as interpreter_module
+from boscli import completer as completer_module
+
+class AutoCompletionTest(unittest.TestCase):
+
+	def setUp(self):
+		parser = parser_module.Parser()
+		self.interpreter = interpreter_module.Interpreter(parser)
+                self.completer = completer_module.Completer(self.interpreter, parser)
+                self.implementation = Stub()
+
+                self.interpreter.add_command(boscli.Command(['sys', 'reboot'], self.implementation.reboot))
+                self.interpreter.add_command(boscli.Command(['sys', 'shutdown'], self.implementation.shutdown))
+                self.interpreter.add_command(boscli.Command(['net', 'show', 'configuration'], self.implementation.show_net_conf))
+                
+        def test_basic_autocompletion(self):
+            assert_that(self.completer.complete(''), has_items('sys ', 'net '))
+            assert_that(self.completer.complete('sy'), has_items('sys '))
+            assert_that(self.completer.complete('sys'), has_items('sys '))
+            assert_that(self.completer.complete('sys r'), has_items('reboot '))
+            assert_that(self.completer.complete('sys reboot'), has_length(0))
+	
