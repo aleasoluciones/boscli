561e777b73a49aada91a027ee2fb707a39eed367
Guillermo Pascual <akira.space@gmail.com>
2014-06-16 15:48:42 +0200

    Alea Soluciones SLL is the author of the package.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='boscli',
       version='0.0.1',
-      author='Eduardo Ferro Aldama',
+      author='Alea Soluciones SLL',
       description ='',
       platforms = 'Linux',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
