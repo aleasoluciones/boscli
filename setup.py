dc0e56182052edc7e7b189b78d4ff7eb3a897fbe
Jaime Gil de Sagredo Luna <jgil@alea-soluciones.com>
2014-07-16 12:01:07 +0200

    Fixed coverage report

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+import mamba.cli
+
+
+class MambaTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import sys
+        sys.argv = ['mamba']
+        mamba.cli.main()
 
 setup(name='boscli',
       version='0.0.1',
@@ -6,4 +23,5 @@ setup(name='boscli',
       description ='',
       platforms = 'Linux',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      cmdclass={'test': MambaTest}
 )
