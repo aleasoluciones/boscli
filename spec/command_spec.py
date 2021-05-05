f0acb96a2c43bfa9150b706f376e4efb39c79720
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2021-05-05 23:40:49 +0200

    Allow to assing an ID for each command

---
+++ spec/command_spec.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from hamcrest import is_
+from doublex import assert_that
+
+from boscli.command import Command
+
+with describe('Command'):
+    with it('Allow to assign a command ID'):
+        command = Command(['k1', 'k2'], cmd_id='cmd_id1')
+
+        assert_that(command.cmd_id, is_('cmd_id1'))
