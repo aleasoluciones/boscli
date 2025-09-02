5171eab9260c9c7cef5b27c26fe4b2e7ca53b544
Eduardo Ferro Aldama <eferro@eferro.net>
2025-09-02 15:54:24 +0200

    Drop Python 2 leftovers

--- boscli/exceptions.py
+++ boscli/exceptions.py
@@ -9,7 +9,7 @@ class NoMatchingCommandFoundError(EvalError):
 class AmbiguousCommandError(EvalError):
     def __init__(self, matching_commands):
         self.matching_commands = matching_commands
-        super(AmbiguousCommandError, self).__init__(matching_commands)
+        super().__init__(matching_commands)
 
 
 class SyntaxError(EvalError):
